1d6f8bbfdc210b12f554838cd6326850d1468888
Your Name <107193947+MohamedaliHammi@users.noreply.github.com>
2022-08-04 08:20:25 +0100

    I add it the score

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const playerText = document.querySelector('#playerText');
 const computerText = document.querySelector('#computerText');
 const choiceBtns = Array.from (document.querySelectorAll('#choiceBtns'));
 const resultText = document.querySelector('#resultText');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
 
-
+let playerPoints = 0;
+let computerPoints = 0;
 let player;
 let computer;
 let result;
 
 choiceBtns.forEach(button => button.addEventListener('click', () => {
     player = button.textContent;
-   randomTurn();
+   computerTurn();
  playerText.textContent = `Player: ${player}`
  computerText.textContent  = `Computer: ${computer}`;
  resultText.textContent = checkWinner();
+
+ showScore();
+ playerScore.textContent = `Player score : ${playerPoints}`;
+ computerScore.textContent = `Computer score : ${computerPoints}`;
+ endGame();
+ 
 
 
 }))
@@ -22,7 +31,7 @@ choiceBtns.forEach(button => button.addEventListener('click', () => {
 
 
 
-function randomTurn(){
+function computerTurn(){
     let random = Math.floor(Math.random() * 3) + 1;
     switch(random){
         case 1:
@@ -47,7 +56,28 @@ function checkWinner(){
     }else if(computer == 'Scissors'){
         return (player == 'Rock')? 'you win' : 'you lose';
     }
+    
+};
+
+function showScore (){
+  if(resultText.textContent === 'you lose'){
+    computerPoints++
+  }else if(resultText.textContent === 'you win'){
+    playerPoints++
+  }
+};
+
+function endGame(){
+    if(playerPoints == 7 && playerPoints > computerPoints){
+        alert('you win')
+        alert('Congrats!')
+        playerPoints = 0;
+        computerPoints = 0;
+    }else if(computerPoints == 7 && computerPoints > playerPoints){
+        alert('you lose')
+        alert('try again!')
+        playerPoints = 0;
+        computerPoints = 0;
+    }
 }
-
-
 
